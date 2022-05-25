f276ea580f85b70f9f4c17ea6ce3aa1053ca8d4a
SddOffice <ssdoffice44@gmail.com>
2022-05-25 11:28:14 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import FormInput from './src/FormInput';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'red',
+      }}>
+      <FormInput />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/FormInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const FormInput = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          backgroundColor: 'red',
+        }}>
+        Hello
+      </Text>
+    </View>
+  );
+};
+
+export default FormInput;
